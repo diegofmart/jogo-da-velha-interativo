793d322198c71f2d8f94cfde424911ef29a52f8f
ShyShy <diegoferrm19@gmail.com>
2026-04-30 21:12:52 -0300

    Update Jogador.java

--- src/main/java/Jogador.java
+++ src/main/java/Jogador.java
@@ -1,1 +1,18 @@
 
+public class Jogador {
+    private char simbolo;
+    private String nome;
+
+    public Jogador(char simbolo, String nome) {
+        this.simbolo = simbolo;
+        this.nome = nome;
+    }
+
+    public char getSimbolo() {
+        return this.simbolo;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+}
